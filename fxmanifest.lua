fef5f71b998c73404598bf791879ac6acd38c7f7
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-01-14 15:30:09 +1100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DonHulieo'
+description 'Don\'s Utility Functions for FiveM'
+version '0.0.1'
+
+server_script {'server/main.lua'}
+
+client_script {'client/main.lua'}
+
+shared_script {'shared/config.lua'}
+
+lua54 'yes'
