1653f23c1ee072e8b987e477e0c040400b59fa2e
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-01-15 16:59:08 +1100

    Add files via upload

--- shared/math.lua
+++ shared/math.lua
@@ -1,3 +1,19 @@
+--------------------------------- Compare Tables ---------------------------------
+
+---@param t1 table
+---@param t2 table
+local function CompareTables(t1, t2)
+    if #t1 ~= #t2 then return false end
+    for k, v in pairs(t1) do
+        if t2[k] ~= v then return false end
+    end
+    return true
+end
+
+exports('CompareTables', function(t1, t2) return CompareTables(t1, t2) end)
+
+--------------------------------- Round Number ---------------------------------
+
 local function RoundNumber(num, numDecimalPlaces)
     if numDecimalPlaces and numDecimalPlaces > 0 then
         local mult = 10^numDecimalPlaces
