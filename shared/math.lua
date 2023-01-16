5755f12dc81149cd5d0b07932c76a689c0542fe5
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-01-16 17:03:06 +1100

    Added GetHeadingBetweenCoords

--- shared/math.lua
+++ shared/math.lua
@@ -29,22 +29,50 @@ exports('RoundNumber', function(num, numDecimalPlaces) return RoundNumber(num, n
 
 ---@param zones table
 ---@return table
-function ConvertToVec2(table)
+local function ConvertToVec2(table)
     return vector2(table.x, table.y)
 end
 
 ---@param table table
 ---@return 'vector3'
-function ConvertToVec3(table)
+local function ConvertToVec3(table)
     return vector3(table.x, table.y, table.z)
 end
 
 ---@param table table
 ---@return 'vector4'
-function ConvertToVec4(table)
+local function ConvertToVec4(table)
     return vector4(table.x, table.y, table.z, table.w)
 end
 
 exports('ConvertToVec2', function(table) return ConvertToVec2(table) end)
 exports('ConvertToVec3', function(table) return ConvertToVec3(table) end)
 exports('ConvertToVec4', function(table) return ConvertToVec4(table) end)
+
+--------------------------------- Get Heading Between Coords ---------------------------------
+
+---@param x1 number
+---@param y1 number
+---@param z1 number
+---@param x2 number
+---@param y2 number
+---@param z2 number
+---@return number
+local function GetHeadingBetweenCoords(x1, y1, z1, x2, y2, z2)
+    local from, to = nil, nil
+    if type(x1) == 'table' and type(y1) == 'table' then
+        from = ConvertToVec3(x1)
+        to = ConvertToVec3(y1)
+    elseif type(x1) == 'vector3' and type(y1) == 'vector3' then
+        from = x1
+        to = y1
+    else
+        from = vector3(x1, y1, z1)
+        to = vector3(x2, y2, z2)
+    end
+    local dx = to.x - from.x
+    local dy = to.y - from.y
+    local heading = math.deg(math.atan2(dy, dx))
+    if heading < 0 then heading = heading + 360 end
+    return heading
+end
