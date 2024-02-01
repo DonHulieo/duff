487fcbcd34b8c2882205b377f1b5e91d080b1295
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-02-01 16:40:30 +1100

    Improve require Fn to Act Like Def Lua
    Now searches through resources files to precache any modules, and acts more like lua default require with relative strings. Also improved error handling to supress major error prints.

--- shared/lib.lua
+++ shared/lib.lua
@@ -1,39 +1,144 @@
 local _ = require
-local Modules = {}
 local type, error = type, error
 local load_resource_file = LoadResourceFile
----@param path string the path to the file to load ie. 'duf.shared.require'
----@return {[string]: function}? @returns the loaded module
-function require(path) -- Credits go to: [overextended](https://github.com/overextended/ox_lib/blob/master/imports/require/shared.lua) & [0xWaleed](https://github.com/citizenfx/fivem/pull/736)
-  local param_type = type(path)
+package = {loaded = {}, path = {}, preload = {}}
+
+---@enum file_ids
+local file_ids = {'file', 'files'}
+---@param resource string
+---@param fn function
+---@param ... any
+---@return integer|string?
+local function ensure_files(resource, fn, ...)
+  for i = 1, #file_ids do
+    local file_id = file_ids[i]
+    local success = fn(resource, file_id, ...)
+    if success then return success end
+  end
+end
+
+---@param res string
+---@param file string
+---@return boolean, function?
+local function safe_load(res, file)
+  return pcall(load, load_resource_file(res, file..'.lua'))
+end
+
+---@param resource string
+local function init_paths(resource)
+  local filLim = ensure_files(resource, GetNumResourceMetadata) - 1
+  for j = 0, filLim do
+    local file = ensure_files(resource, GetResourceMetadata, j) --[[@as string]]
+    local name = file:match('^(.-)%.lua$') or file:match('^(.-)%*%*$')
+    if name then
+      local path = name:gsub('%.', '/')
+      name = resource..':'..name
+      package.path[name] = {resource, path}
+      local loaded, func = safe_load(resource, path)
+      if loaded then package.preload[name] = func end
+    end
+  end
+end
+
+local function find_paths()
+  local resLim = GetNumResources() - 1
+  for i = 0, resLim do
+    local res = GetResourceByFindIndex(i)
+    init_paths(res)
+  end
+end
+
+find_paths() -- Find As Many Paths As Possible On Startup
+
+---@enum contexts
+local contexts = {'shared', 'server', 'client'}
+---@param name string
+---@return string[]? path
+local function find_path(name)
+  local parts = {}
+  local path = {}
+  for part in name:gmatch('[^.]+') do
+    parts[#parts + 1] = part
+  end
+  for i = 1, #parts do
+    local part = parts[i]:gsub('%.', '/')
+    local found = package.path[part]
+    if found then return found end
+    for _, val in pairs(package.path) do
+      local res, file = val[1], val[2]
+      if file:match('%'..part..'$') then
+        local new_path = file
+        local loaded, func = safe_load(res, new_path)
+        if loaded then
+          path = {res, new_path, func}
+          break
+        end
+      elseif file:match('%*%*') and file:match('%*$') then
+        for j = 1, #contexts do
+          local new_path = file:gsub('%*%*', contexts[j]):gsub('%*$', part)
+          local loaded, func = safe_load(res, new_path)
+          if loaded then
+            path = {res, new_path, func}
+            break
+          end
+        end
+      elseif file:match('%*%*') then
+        local new_path = file:gsub('%*%*', part)
+        local loaded, func = safe_load(res, new_path)
+        if loaded then
+          path = {res, new_path, func}
+          break
+        end
+      elseif file:match('%*$') then
+        local new_path = file:gsub('%*$', part)
+        local loaded, func = safe_load(res, new_path)
+        if loaded then
+          path = {res, new_path, func}
+          break
+        end
+      end
+    end
+  end
+  if #path > 0 then
+    name = path[1]..':'..path[2]
+    package.path[name] = {path[1], path[2]}
+    package.preload[name] = path[3]
+    return package.path[name]
+  end
+end
+
+---@param name string @The name of the module to require. This can be a path, or a module name. If a path is provided, it must be relative to the resource root.
+---@return {[string]: any} module
+function require(name)
+  local param_type = type(name)
   if param_type ~= 'string' then error('bad argument #1 to \'require\' (string expected, got ' ..param_type.. ')', 2) end
-  if Modules[path] then print('Module already loaded: ' .. path) return Modules[path] end
-  local resource, file = path:match('^(.-)%.(.*)$')
-  if not resource then error('bad argument #1 to \'require\' (invalid path)', 2) end
-	local content = load_resource_file(resource, file:gsub('%.', '/')..'.lua')
-	if not content then error('Failed to load module: ' .. path, 2) end
-	local func, err = load(content)
-	if not func then error(err, 2) end
-  local ret = func()
-  Modules[path] = ret
-  return ret
+  local path = find_path(name)
+  if not path then error('bad argument #1 to \'require\' (invalid path)', 2) end
+  name = path[1]..':'..path[2]
+  if not package.preload[name] then error('bad argument #1 to \'require\' (no file found)', 2) end
+  local loaded, module = pcall(package.preload[name])
+  if not loaded then error('bad argument #1 to \'require\' (error loading file)', 2) end
+  package.loaded[name] = module
+  return package.loaded[name]
 end
 
 exports('require', require)
--- exports('ImportObject', function()
---   return require 'duf.shared.import'
--- end)
 
----@param resource string
-local function clearModules(resource)
-  if resource ~= GetCurrentResourceName() then return end
-  Modules = {}
+local function deinit_package(resource)
+  for name, path in pairs(package.path) do
+    if path[1] == resource then
+      package.path[name] = nil
+      package.preload[name] = nil
+      package.loaded[name] = nil
+    end
+  end
 end
 
-AddEventHandler('onResourceStop', clearModules)
+AddEventHandler('onResourceStart', init_paths)
+AddEventHandler('onResourceStop', deinit_package)
 
 ---@module 'duf.shared.array'
-local array = require 'duf.shared.array'
+local array = require 'shared.array'
 
 ---@param param any
 ---@param type_name string|string[]
