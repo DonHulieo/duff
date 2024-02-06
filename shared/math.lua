6ac98083f9c92a7faacb845dfc5e492fec8f0fc6
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-02-06 20:48:00 +1100

    Add Error Messaging

--- shared/math.lua
+++ shared/math.lua
@@ -5,6 +5,7 @@
 ---@field random fun(m: integer, n: integer?): integer?
 ---@field timer fun(time: integer, limit: integer): boolean
 local CMath do
+  local check_type = CheckType
   local math = math
   local math_floor = math.floor
   local random_seed, math_random = math.randomseed, math.random
@@ -16,15 +17,20 @@ local CMath do
   ---@param val number
   ---@param min number
   ---@param max number
-  ---@return number
+  ---@return number?
   local function clamp(val, min, max)
+    if not check_type(val, 'number', 'clamp', 1, 5) then return end
+    if not check_type(min, 'number', 'clamp', 2, 5) then return end
+    if not check_type(max, 'number', 'clamp', 3, 5) then return end
     return val < min and min or val > max and max or val
   end
 
   ---@param val number
   ---@param increment integer?
-  ---@return integer
+  ---@return integer?
   local function round(val, increment)
+    if not check_type(val, 'number', 'round', 1, 5) then return end
+    if increment and not check_type(increment, 'number', 'Round', 2, 5) then return end
     if increment then return round(val / increment) * increment end
     return val >= 0 and math_floor(val + .5) or math_floor(val - .5)
   end
@@ -40,15 +46,17 @@ local CMath do
   ---@param n integer? Maximum if m and n are provided
   ---@return integer?
   local function random(m, n)
-    if not m then return end
+    if not check_type(m, 'number', 'random', 1, 5) then return end
     m, n = not n and 1 or m, not n and m or n
     return math_floor(math_random() * (n - m + 1) + m)
   end
 
   ---@param time integer
   ---@param limit integer
-  ---@return boolean
+  ---@return boolean?
   local function timer(time, limit)
+    if not check_type(time, 'number', 'timer', 1, 5) then return end
+    if not check_type(limit, 'number', 'timer', 2, 5) then return end
     local current = game_timer()
     return current - time > limit
   end
