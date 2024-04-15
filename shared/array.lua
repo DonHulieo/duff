abbe62eabe1dafb9ad3a75964df0c4a21e61999d
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-04-16 09:51:22 +1000

    Improve Annotations & Class Usage

--- shared/array.lua
+++ shared/array.lua
@@ -2,34 +2,37 @@
 ---@field private __actions table
 ---@field private __type string
 ---@field new fun(self: any[]?): self|any? Creates a new array.
----@field isarray fun(tbl: table): boolean, string Checks if the table is an array.
+---@field insert fun(self: CArray, index: integer, value: any): CArray Inserts an element at the specified index.
+---@field remove fun(self: CArray, index: integer): any? Removes and returns the element at the specified index.
+---@field sort fun(self: CArray, fn: function): CArray Sorts the elements of the array.
+---@field concat fun(self: CArray, sep: string): string Concatenates all elements of the array with an optional separator.
+---@field isarray fun(tbl: table): boolean? Checks if a table is an array.
 ---@field push fun(self: CArray, arg: any?, ...: any?): CArray Adds one or more elements to the end of the array.
 ---@field pusharray fun(self: CArray, tbl: table): CArray Adds all elements from a table to the end of the array.
----@field peek fun(self: CArray, index: integer?): any Returns the element at the specified index in the array without removing it.
+---@field peek fun(self: CArray, index: integer?): any Returns the element at the specified index without removing it.
 ---@field peekarray fun(self: CArray, index: integer?): CArray Returns a new array containing the elements from the specified index to the end of the array.
----@field pop fun(self: CArray, index: integer?): any, CArray Removes and returns the element at the specified index in the array.
+---@field pop fun(self: CArray, index: integer?): any, CArray Removes and returns the element at the specified index.
 ---@field poparray fun(self: CArray, index: integer?): CArray Removes and returns a new array containing the elements from the specified index to the end of the array.
 ---@field contains fun(self: CArray, key: integer?, value: any?): boolean? Checks if the array contains a specific element or key or key-value pair.
----@field find fun(self: CArray, fn: function): integer? Searches for the first element in the array that satisfies a given condition and returns its index.
+---@field find fun(self: CArray, fn: function): integer? Searches for the first element that satisfies a given condition and returns its index.
 ---@field copy fun(self: CArray): CArray Creates a shallow copy of the array.
 ---@field foldleft fun(self: CArray, fn: function, arg: any?): CArray Applies a function to each element from left to right, accumulating a result.
 ---@field foldright fun(self: CArray, fn: function, arg: any?): CArray Applies a function to each element from right to left, accumulating a result.
----@field setenum fun(self: CArray?): table Creates a read-only table that can be used for enumeration.
----@field map fun(self: CArray, fn: function, inPlace: boolean?): CArray Applies a function to each element in the array and returns a new array with the results.
+---@field setenum fun(self: CArray?): table Creates a read-only array that can be used for enumeration.
+---@field map fun(self: CArray, fn: function, inPlace: boolean?): CArray Applies a function to each element and returns a new array with the results.
 ---@field filter fun(self: CArray, fn: function, inPlace: boolean?): CArray Returns a new array containing only the elements that satisfy a given condition.
----@field foreach fun(self: CArray, fn: function) Executes a function for each element in the array.
+---@field foreach fun(self: CArray, fn: function) Executes a function for each element across the array.
 ---@field reverse fun(self: CArray): CArray Reverses the order of elements.
 local CArray do
   local table = table
-  CArray = {__actions = {}, __type = 'array'}
-  CArray.__actions.__index = CArray
+  CArray = {__actions = {__index = CArray}, __type = 'array'}
   CArray.insert = table.insert
   CArray.remove = table.remove
   CArray.sort = table.sort
   CArray.concat = table.concat
   local min = math.min
   ---@module 'shared.debug'
-  local debug = require('shared.debug')
+  local debug = require 'shared.debug'
   local check_type = debug.checktype
 
   ---@param tbl table
@@ -40,26 +43,13 @@ local CArray do
     return check_type(tbl, 'table', fn, arg_no)
   end
 
-  ---@param tbl table
-  ---@param fn function
-  ---@param arg_no integer?
-  ---@return boolean
-  local function is_true_array(tbl, fn, arg_no)
-    local len = #tbl
-    for k, _ in pairs(tbl) do
-      if type(k) ~= 'number' or k < 1 or k > len then return false end
-    end
-    return true
-  end
-
   ---@param self table
-  ---@return CArray?
+  ---@return CArray
   local function new(self)
     self = self or {}
-    if not is_table(self, new, 1) then return end
-    if not is_true_array(self, new, 1) then return end
+    if not is_table(self, new, 1) then return self end
     for k, v in pairs(CArray) do self[k] = v end
-    self.__index = CArray
+    setmetatable(self, {__index = CArray.__actions, __type = 'array'})
     return self
   end
 
@@ -111,7 +101,7 @@ local CArray do
   ---@return any[]
   local function peekArray(self, index)
     index = index or 1
-    local res = CArray{}
+    local res = new{}
     local len = #self
     for i = 1, min(index, len) do res[i] = self[len - i + 1] end
     return res
@@ -124,7 +114,7 @@ local CArray do
     index = index or 1
     if index <= 0 or index > #self then return end
     local value = self[index]
-    value = self:remove(index)
+    value = CArray.remove(self, index)
     return value and value, value and pop(self, index - 1)
   end
 
@@ -133,8 +123,8 @@ local CArray do
   ---@return any[]
   local function popArray(self, index)
     index = index or 1
-    local res = CArray{}
-    for i = 1, min(index, #self) do res[i] = self:remove() end
+    local res = new{}
+    for i = 1, min(index, #self) do res[i] = CArray.remove(self) end
     return res
   end
 
@@ -165,7 +155,7 @@ local CArray do
   ---@param self CArray
   ---@return any[]
   local function copy(self)
-    local res = CArray{}
+    local res = new{}
     for i = 1, #self do res[i] = self[i] end
     return res
   end
@@ -205,7 +195,7 @@ local CArray do
   ---@param inPlace boolean?
   ---@return CArray
   local function map(self, fn, inPlace)
-    local res = inPlace and self or CArray{}
+    local res = inPlace and self or new{}
     for i = 1, #self do res[i] = fn(self[i]) end
     return res
   end
@@ -218,11 +208,11 @@ local CArray do
     if inPlace then
       local i = 1
       while i <= #self do
-        if not fn(self[i], i) then self:remove(i) else i += 1 end
+        if not fn(self[i], i) then CArray.remove(self, i) else i += 1 end
       end
       return self
     end
-    local res = CArray{}
+    local res = new{}
     for i = 1, #self do
       local val = self[i]
       if fn(val) then res[#res + 1] = val end
@@ -249,6 +239,7 @@ local CArray do
     return self
   end
 
+  CArray.new = new
   CArray.isarray = isArray
   CArray.push = push
   CArray.pusharray = pushArray
@@ -266,7 +257,6 @@ local CArray do
   CArray.filter = filter
   CArray.foreach = forEach
   CArray.reverse = reverse
-  CArray.new = new
 
   return CArray
 end
