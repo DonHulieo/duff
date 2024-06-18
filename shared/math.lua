623b01dea814e99f01fa842885ddd8bdd2808407
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-06-19 01:08:17 +1000

    chore(shared/math): Use `trace` For Error Handling

--- shared/math.lua
+++ shared/math.lua
@@ -1,21 +1,22 @@
 local _math = math
 ---@class math: mathlib
----@field between fun(val: number, min: number, max: number): boolean? returns `true` if `val` is between `min` and `max`, `false` otherwise.
----@field clamp fun(val: number, min: number, max: number): number `val` clamped between `min` and `max`.
----@field gcd fun(num: integer, den: integer): integer Greatest Common Divisor of `num` and `den`.
----@field ishalf fun(val: number): boolean returns true if `val` is a half number. <br> e.g. `1.5`, `-1.5`
----@field isint fun(val: number): boolean returns true if `val` is an integer.
----@field ispos fun(val: number): boolean returns true if `val` is positive.
----@field round fun(val: number, increment: integer?): integer `val` rounded to the nearest integer. <br> `increment` if provided, rounds to the nearest multiple of `increment`.
----@field seedrng fun(): integer? seeds the random number generator with a unique seed based on the current time.
----@field random fun(m: integer , n: integer?): integer? `m` minimum or maximum (from 1). <br> `n` maximum if m and n are provided. <br> returns a random number between `m` and `n`.
----@field timer fun(time: integer, limit: integer): boolean checks if `time` has exceeded `limit` in milliseconds.
----@field tofloat fun(num: integer, den: integer): number `num` is the numerator. <br> `den` is the denominator. <br> returns the rational number as a float.
----@field toint fun(val: number): integer converts a float to integer following the round half away from zero rule. <br> e.g. `1.5 -> 2`, `-1.5 -> -2` <br> [Rounding Half Toward Zero](https://en.wikipedia.org/wiki/Rounding#Rounding_half_toward_zero)
----@field toratio fun(float: number, precision: number?): integer, integer `float` is the float to convert. <br> `precision` is the maximum error allowed in the conversion. <br> returns the float as a rational number.
+---@field between fun(val: number, min: number, max: number): boolean? Returns `true` if `val` is between `min` and `max`, `false` otherwise.
+---@field clamp fun(val: number, min: number, max: number): number? `val` clamped between `min` and `max`.
+---@field gcd fun(num: integer, den: integer): integer? Greatest Common Divisor of `num` and `den`.
+---@field ishalf fun(val: number): boolean? Returns true if `val` is a half number. <br> e.g. `1.5`, `-1.5`
+---@field isint fun(val: number): boolean? Returns true if `val` is an integer.
+---@field sign fun(val: number): integer? Returns `1` if `val` is positive, `-1` if `val` is negative.
+---@field round fun(val: number, increment: integer?): integer? `val` rounded to the nearest integer. <br> `increment` if provided, rounds to the nearest multiple of `increment`.
+---@field seedrng fun(): integer? Seeds the random number generator with a unique seed based on the current time.
+---@field random fun(m: integer , n: integer?): integer? `m` minimum or maximum (from 1). <br> `n` maximum if m and n are provided. <br> Returns a random number between `m` and `n`.
+---@field timer fun(time: integer, limit: integer): boolean? Checks if `time` has exceeded `limit` in milliseconds.
+---@field tofloat fun(num: integer, den: integer): number? `num` is the numerator. <br> `den` is the denominator. <br> Returns the rational number as a float.
+---@field toint fun(val: number): integer Converts a float to integer following the round half away from zero rule. <br> e.g. `1.5 -> 2`, `-1.5 -> -2` <br> [Rounding Half Toward Zero](https://en.wikipedia.org/wiki/Rounding#Rounding_half_toward_zero)
+---@field toratio fun(float: number, precision: number?): integer?, integer? `float` is the float to convert. <br> `precision` is the maximum error allowed in the conversion. <br> Returns the float as a rational number.
 local math do
   local require = require
-  local check_type = require('duff.shared.debug').checktype
+  local type = type
+  local trace = require 'duff.shared.trace'
   local math_floor, math_abs = _math.floor, _math.abs
   local random_seed, math_random = _math.randomseed, _math.random
   local huge = _math.huge
@@ -29,7 +30,9 @@ local math do
   ---@param max number The maximum value.
   ---@return boolean? is_between `true` if `val` is between `min` and `max`, `false` otherwise.
   local function between(val, min, max)
-    if not check_type(val, 'number', between, 1) or not check_type(min, 'number', between, 2) or not check_type(max, 'number', between, 3) then return end
+    if not val or type(val) ~= 'number' then trace('error', 'bad argument #1 to \'between\' (number expected, got '..type(val)..')') return end
+    if not min or type(min) ~= 'number' then trace('error', 'bad argument #2 to \'between\' (number expected, got '..type(min)..')') return end
+    if not max or type(max) ~= 'number' then trace('error', 'bad argument #3 to \'between\' (number expected, got '..type(max)..')') return end
     return val >= min and val <= max
   end
 
@@ -38,14 +41,25 @@ local math do
   ---@param max number The maximum value.
   ---@return number? clamped `val` clamped between `min` and `max`.
   local function clamp(val, min, max)
-    if not check_type(val, 'number', clamp, 1) or not check_type(min, 'number', clamp, 2) or not check_type(max, 'number', clamp, 3) then return end
+    if not val or type(val) ~= 'number' then trace('error', 'bad argument #1 to \'clamp\' (number expected, got '..type(val)..')') return end
+    if not min or type(min) ~= 'number' then trace('error', 'bad argument #2 to \'clamp\' (number expected, got '..type(min)..')') return end
+    if not max or type(max) ~= 'number' then trace('error', 'bad argument #3 to \'clamp\' (number expected, got '..type(max)..')') return end
     return val < min and min or val > max and max or val
+  end
+
+  ---@param val integer|number The value to check.
+  ---@return boolean? is_integer `true` if `val` is an integer.
+  local function is_integer(val)
+    if not val or type(val) ~= 'number' then trace('error', 'bad argument #1 to \'isint\' (number expected, got '..type(val)..')') return end
+    return val % 1 == 0
   end
 
   ---@param num integer The numerator.
   ---@param den integer The denominator.
-  ---@return integer gcd The Greatest Common Divisor of `num` and `den`.
+  ---@return integer? gcd The Greatest Common Divisor of `num` and `den`.
   local function g_c_d(num, den)
+    if not num or not is_integer(num) then trace('error', 'bad argument #1 to \'gcd\' (integer expected, got '..type(num)..')') return end
+    if not den or not is_integer(den) then trace('error', 'bad argument #2 to \'gcd\' (integer expected, got '..type(den)..')') return end
     while den ~= 0 do
       num, den = den, num % den
     end
@@ -53,35 +67,33 @@ local math do
   end
 
   ---@param val number The value to check.
-  ---@return boolean is_half `true` if `val` is a half number. <br> e.g. `1.5`, `-1.5`.
+  ---@return boolean? is_half `true` if `val` is a half number. <br> e.g. `1.5`, `-1.5`.
   local function is_half(val)
+    if not val or type(val) ~= 'number' then trace('error', 'bad argument #1 to \'ishalf\' (number expected, got '..type(val)..')') return end
     return val % 1 == .5
   end
 
-  ---@param val integer|number The value to check.
-  ---@return boolean is_integer `true` if `val` is an integer.
-  local function is_integer(val)
-    return val % 1 == 0
-  end
-
   ---@param val number The value to check.
-  ---@return boolean is_positive `true` if `val` is positive.
-  local function is_positive(val)
-    return val > 0
+  ---@return integer? sign `1` if `val` is positive, `-1` if `val` is negative.
+  local function sign(val)
+    if not val or type(val) ~= 'number' then trace('error', 'bad argument #1 to \'sign\' (number expected, got '..type(val)..')') return end
+    return val > 0 and 1 or -1
   end
 
   ---@param val number The value to convert.
-  ---@return integer int The converted value.
+  ---@return integer? int The converted value.
   local function to_int(val) -- Converts a `float` to `integer` following the round half away from zero rule. <br> eg. `1.5 -> 2`, `-1.5 -> -2` <br> [Rounding Half Toward Zero](https://en.wikipedia.org/wiki/Rounding#Rounding_half_toward_zero)
-    local half, pos = val % 1 >= .5, is_positive(val)
+    if not val or type(val) ~= 'number' then trace('error', 'bad argument #1 to \'toint\' (number expected, got '..type(val)..')') return end
+    local half, pos = val % 1 >= .5, sign(val) == 1
     return half and (pos and math_floor(val+.5) or math_floor(val-.5)) or math_floor(val)
   end
 
   ---@param val number The value to round.
-  ---@param increment integer? The increment to round to.
-  ---@return number|integer? rounded `val` rounded to the nearest integer. <br> `increment` if provided, rounds to the nearest multiple of `increment`.
+  ---@param increment number? The increment to round to.
+  ---@return number|integer? rounded `val` rounded to the nearest number. <br> `increment` if provided, rounds to the nearest multiple of `increment`.
   local function round(val, increment)
-    if not check_type(val, 'number', round, 1) or increment and not check_type(increment, 'number', round, 2) then return end
+    if not val or type(val) ~= 'number' then trace('error', 'bad argument #1 to \'round\' (number expected, got '..type(val)..')') return end
+    if increment and (type(increment) ~= 'number' or increment < 1) then trace('error', 'bad argument #2 to \'round\' (number greater than 0 expected, got '..type(increment)..')') return end
     if increment then return round(val / increment) * increment end
     return to_int(val)
   end
@@ -97,7 +109,8 @@ local math do
   ---@param n integer? Maximum if m and n are provided.
   ---@return integer? number A random number between `m` and `n`.
   local function random(m, n)
-    if not check_type(m, 'number', random, 1) then return end
+    if not m or not is_integer(m) then trace('error', 'bad argument #1 to \'random\' (integer expected, got '..type(m)..')') return end
+    if n and not is_integer(n) then trace('error', 'bad argument #2 to \'random\' (integer expected, got '..type(n)..')') return end
     m, n = not n and 1 or m, not n and m or n
     return math_floor(math_random() * (n - m + 1) + m)
   end
@@ -106,23 +119,28 @@ local math do
   ---@param limit integer The limit to check against.
   ---@return boolean? has_exceeded `true` if `time` has exceeded `limit` in milliseconds.
   local function timer(time, limit)
-    if not check_type(time, 'number', timer, 1) or not check_type(limit, 'number', timer, 2) then return end
+    if not time or not is_integer(time) then trace('error', 'bad argument #1 to \'timer\' (integer expected, got '..type(time)..')') return end
+    if not limit or not is_integer(limit) then trace('error', 'bad argument #2 to \'timer\' (integer expected, got '..type(limit)..')') return end
     local current = game_timer()
     return current - time > limit
   end
 
   ---@param num integer The numerator.
   ---@param den integer? The denominator.
-  ---@return number float the `num` or `num/den` as a float.
+  ---@return number? float The `num` or `num/den` as a float.
   local function to_float(num, den) -- Converts an integer to float if `den` is not provided. <br> Converts a rational number to a float if `den` is provided.
+    if not num or not is_integer(num) then trace('error', 'bad argument #1 to \'tofloat\' (integer expected, got '..type(num)..')') return end
+    if den and not is_integer(den) then trace('error', 'bad argument #2 to \'tofloat\' (integer expected, got '..type(den)..')') return end
     return not den and num + 0.0 or num / den
   end
 
   ---@param float number The float to convert.
   ---@param precision number? The maximum error allowed in the conversion.
-  ---@return integer num, integer den The float as a rational number.
+  ---@return integer? num, integer? den The float as a rational number.
   local function to_rational(float, precision)
-    local is_neg = not is_positive(float)
+    if not float or type(float) ~= 'number' then trace('error', 'bad argument #1 to \'toratio\' (number expected, got '..type(float)..')') return end
+    if precision and (type(precision) ~= 'number' or precision < 1e-10) then trace('error', 'bad argument #2 to \'toratio\' (number greater than 1e-10 expected, got '..type(precision)..')') return end
+    local is_neg = sign(float) == -1
     float = is_neg and -float or float
     precision =  precision or 1e-10
     local h1, h2, k1, k2 = 1, 0, 0, 1
@@ -147,7 +165,7 @@ local math do
   math.gcd = g_c_d
   math.ishalf = is_half
   math.isint = is_integer
-  math.ispos = is_positive
+  math.sign = sign
   math.round = round
   math.seedrng = seed_rng
   math.random = random
