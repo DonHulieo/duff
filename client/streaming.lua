700876cbe330ef792440ce754694f1af01982694
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-06-21 12:47:27 +1000

    fix(client/streaming): Async Error Handling
    Async function lib wasn't handling errors as expected, that should be fixed by defining them as their own functions before object creation.

--- client/streaming.lua
+++ client/streaming.lua
@@ -1,11 +1,16 @@
 ---@class streaming
----@field async table<string, function> Asynchronous functions for loading streaming assets.
 ---@field loadanimdict fun(dict: string): boolean Loads an animation dictionary.
+---@field async.loadanimdict fun(dict: string): boolean? Loads an animation dictionary without blocking the main thread.
 ---@field loadanimset fun(set: string): boolean Loads an animation set.
+---@field async.loadanimset fun(set: string): boolean? Loads an animation set without blocking the main thread.
 ---@field loadcollision fun(model: string|number): boolean Loads collision for a model.
+---@field async.loadcollision fun(model: string|number): boolean? Loads collision for a model without blocking the main thread.
 ---@field loadipl fun(ipl: string): boolean Loads an IPL.
+---@field async.loadipl fun(ipl: string): boolean? Loads an IPL without blocking the main thread.
 ---@field loadmodel fun(model: string|number): boolean Loads a model.
+---@field async.loadmodel fun(model: string|number): boolean? Loads a model without blocking the main thread.
 ---@field loadptfx fun(fx: string): boolean Loads a particle effect asset.
+---@field async.loadptfx fun(fx: string): boolean? Loads a particle effect asset without blocking the main thread.
 local streaming do
   local game_timer = GetGameTimer
   local require = require
@@ -41,6 +46,13 @@ local streaming do
     return load_asset(dict, has_anim_dict_loaded, request_anim_dict)
   end
 
+  ---@param dict string The animation dictionary to load.
+  ---@return boolean? loaded Whether the animation dictionary was loaded.
+  local function async_req_anim_dict(dict)
+    if not dict or type(dict) ~= 'string' then error('bad argument #1 to \'%s\' (string expected, got '..type(dict)..')', 0) end
+    return async(req_anim_dict, dict)
+  end
+
   ---@param set string The animation set to load.
   ---@return boolean loaded Whether the animation set was loaded.
   local function req_anim_set(set)
@@ -48,14 +60,31 @@ local streaming do
     return load_asset(set, has_anim_set_loaded, request_anim_set)
   end
 
+  ---@param set string The animation set to load.
+  ---@return boolean? loaded Whether the animation set was loaded.
+  local function async_req_anim_set(set)
+    if not set or type(set) ~= 'string' then error('bad argument #1 to \'%s\' (string expected, got '..type(set)..')', 0) end
+    return async(req_anim_set, set)
+  end
+
   ---@param model string|number The model to load collision for.
   ---@return boolean loaded Whether the collision was loaded.
   local function req_collision(model)
     local param_type = type(model)
-    if not model or (param_type ~= 'string' or param_type ~= 'number') then error('bad argument #1 to \'%s\' (string or number expected, got '..param_type..')', 0) end
-    if not is_model_in_cd(model) or not is_model_in_valid(model) then error('bad argument #1 to \'%s\' (invalid model requested: '..tostring(model)..')', 0) end
+    if not model or (param_type ~= 'string' and param_type ~= 'number') then error('bad argument #1 to \'%s\' (string or number expected, got '..param_type..')', 0) end
     model = type(model) == 'number' and model or joaat(model)
+    if not is_model_in_cd(model) or not is_model_in_valid(model) then error('bad argument #1 to \'%s\' (invalid model requested: '..tostring(model)..')', 0) end
     return load_asset(model, has_collision_for_model_loaded, request_collision_for_model)
+  end
+
+  ---@param model string|number The model to load collision for.
+  ---@return boolean? loaded Whether the collision was loaded.
+  local function async_req_collision(model)
+    local param_type = type(model)
+    if not model or (param_type ~= 'string' and param_type ~= 'number') then error('bad argument #1 to \'%s\' (string or number expected, got '..param_type..')', 0) end
+    model = type(model) == 'number' and model or joaat(model)
+    if not is_model_in_cd(model) or not is_model_in_valid(model) then error('bad argument #1 to \'%s\' (invalid model requested: '..tostring(model)..')', 0) end
+    return async(req_collision, model)
   end
 
   ---@param ipl string The IPL to load.
@@ -65,14 +94,31 @@ local streaming do
     return load_asset(ipl, is_ipl_active, request_ipl)
   end
 
+  ---@param ipl string The IPL to load.
+  ---@return boolean? loaded Whether the IPL was loaded.
+  local function async_req_ipl(ipl)
+    if not ipl or type(ipl) ~= 'string' then error('bad argument #1 to \'%s\' (string expected, got '..type(ipl)..')', 0) end
+    return async(req_ipl, ipl)
+  end
+
   ---@param model string|number The model to load.
   ---@return boolean loaded Whether the model was loaded.
   local function req_model(model)
     local param_type = type(model)
-    if not model or (param_type ~= 'string' or param_type ~= 'number') then error('bad argument #1 to \'%s\' (string or number expected, got '..param_type..')', 0) end
-    if not is_model_in_cd(model) or not is_model_in_valid(model) then error('bad argument #1 to \'%s\' (invalid model requested: '..tostring(model)..')', 0) end
+    if not model or (param_type ~= 'string' and param_type ~= 'number') then error('bad argument #1 to \'%s\' (string or number expected, got '..param_type..')', 0) end
     model = type(model) == 'number' and model or joaat(model)
+    if not is_model_in_cd(model) or not is_model_in_valid(model) then error('bad argument #1 to \'%s\' (invalid model requested: '..tostring(model)..')', 0) end
     return load_asset(model, has_model_loaded, request_model)
+  end
+
+  ---@param model string|number The model to load.
+  ---@return boolean? loaded Whether the model was loaded.
+  local function async_req_model(model)
+    local param_type = type(model)
+    if not model or (param_type ~= 'string' and param_type ~= 'number') then error('bad argument #1 to \'%s\' (string or number expected, got '..param_type..')', 0) end
+    model = type(model) == 'number' and model or joaat(model)
+    if not is_model_in_cd(model) or not is_model_in_valid(model) then error('bad argument #1 to \'%s\' (invalid model requested: '..tostring(model)..')', 0) end
+    return async(req_model, model)
   end
 
   ---@param fx string The particle effect asset to load.
@@ -82,32 +128,21 @@ local streaming do
     return load_asset(fx, has_named_ptfx_asset_loaded, request_named_ptfx_asset)
   end
 
+  ---@param fx string The particle effect asset to load.
+  ---@return boolean? loaded Whether the particle effect asset was loaded.
+  local function async_req_ptfx(fx)
+    if not fx or type(fx) ~= 'string' then error('bad argument #1 to \'%s\' (string expected, got '..type(fx)..')', 0) end
+    return async(req_ptfx, fx)
+  end
+
   return {
     async = {
-      -- Loads an animation dictionary without blocking the main thread.
-      ---@param dict string The animation dictionary to load.
-      ---@return boolean? loaded Whether the animation dictionary was loaded.
-      loadanimdict = function(dict) return async(req_anim_dict, dict) end,
-      -- Loads an animation set without blocking the main thread.
-      ---@param set string The animation set to load.
-      ---@return boolean? loaded Whether the animation set was loaded.
-      loadanimset = function(set) return async(req_anim_set, set) end,
-      -- Loads collision for a model without blocking the main thread.
-      ---@param model string|number The model to load collision for.
-      ---@return boolean? loaded Whether the collision was loaded.
-      loadcollision = function(model) return async(req_collision, model) end,
-      -- Loads an IPL without blocking the main thread.
-      ---@param ipl string The IPL to load.
-      ---@return boolean? loaded Whether the IPL was loaded.
-      loadipl = function(ipl) return async(req_ipl, ipl) end,
-      -- Loads a model without blocking the main thread.
-      ---@param model string|number The model to load.
-      ---@return boolean? loaded Whether the model was loaded.
-      loadmodel = function(model) return async(req_model, model) end,
-      -- Loads a particle effect asset without blocking the main thread.
-      ---@param fx string The particle effect asset to load.
-      ---@return boolean? loaded Whether the particle effect asset was loaded.
-      loadptfx = function(fx) return async(req_ptfx, fx) end
+      loadanimdict = async_req_anim_dict,
+      loadanimset = async_req_anim_set,
+      loadcollision = async_req_collision,
+      loadipl = async_req_ipl,
+      loadmodel = async_req_model,
+      loadptfx = async_req_ptfx
     },
     loadanimdict = req_anim_dict,
     loadanimset = req_anim_set,
