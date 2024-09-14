a1b8cff693cfb2e7e4951f57f5fbb8afd023f8de
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-09-14 12:00:13 +1000

    fix(server/init)!: `check_version` in a new thread
    > ![CAUTION]
    > BREAKING CHANGE: `check_version` now has no return values, and executes fully in function.

--- server/init.lua
+++ server/init.lua
@@ -7,25 +7,24 @@ SetConvarReplicated('locale', GetConvar('locale', 'en')) -- Ensure locale is rep
 ---@param version string? The current version of the resource.
 ---@param git string The github username or organization name.
 ---@param repo string? The github repository name. <br> If not provided, the resource name will be used.
----@return promise? version_check A promise that resolves with the resource and version if an update is available, or rejects with an error message.
 local function check_resource_version(resource, version, git, repo)
-  resource = resource or get_invoking_res()
-  version = version or get_res_meta(resource, 'version', 0)
-  local version_check = promise.new()
-  if not version then version_check:reject(('^1Unable to determine current resource version for `%s` ^0'):format(resource)) return version_check end
-  version = version:match('%d+%.%d+%.%d+')
-  PerformHttpRequest(('https://api.github.com/repos/%s/%s/releases/latest'):format(git, repo or resource), function(status, response)
-    local reject = ('^1Unable to check for updates for `%s` ^0'):format(resource)
-    if status ~= 200 then version_check:reject(reject) return end
-    response = json.decode(response)
-    if response.prerelease then version_check:reject(reject) return end
-    local latest = response.tag_name:match('%d+%.%d+%.%d+')
-    if not latest then version_check:reject(reject) return
-    elseif latest == version then version_check:reject(('^2`%s` is running latest version.^0'):format(resource)) return end
-    local cv, lv = version:gsub('%D', ''), latest:gsub('%D', '')
-    if cv < lv then version_check:resolve({resource = resource, version = version}) end
-  end, 'GET')
-  return version_check
+  SetTimeout(1000, function()
+    resource = resource or get_invoking_res()
+    version = version or get_res_meta(resource, 'version', 0)
+    if not version then return print(('^1Unable to determine current resource version for `%s` ^0'):format(resource)) end
+    version = version:match('%d+%.%d+%.%d+')
+    PerformHttpRequest(('https://api.github.com/repos/%s/%s/releases/latest'):format(git, repo or resource), function(status, response)
+      local reject = ('^1Unable to check for updates for `%s` ^0'):format(resource)
+      if status ~= 200 then return print(reject) end
+      response = json.decode(response)
+      if response.prerelease then return print(reject)end
+      local latest = response.tag_name:match('%d+%.%d+%.%d+')
+      if not latest then return print(reject)
+      elseif latest == version then return print(('^2`%s` is running latest version.^0'):format(resource)) end
+      local cv, lv = version:gsub('%D', ''), latest:gsub('%D', '')
+      if cv < lv then return print(('^3[duff]^7 - ^1An update is available for `%s` (current version: %s)\r\n^3[duff]^7 - ^5Please download the latest version from the github release page.^7\r\n%s'):format(resource, version, response.html_url)) end
+    end, 'GET')
+  end)
 end
 
 exports('checkversion', check_resource_version)
@@ -33,14 +32,8 @@ exports('checkversion', check_resource_version)
 ---@param resource string
 local function init_duff(resource)
   if resource ~= 'duff' then return end
-  SetTimeout(2000, function()
-    local version = get_res_meta('duff', 'version', 0)
-    check_resource_version('duff', version, 'donhulieo'):next(function(data)
-      print(('^3[duff]^7 - ^1An update is available for `%s` (current version: %s)\r\n^3[duff]^7 - ^5Please download the latest version from the github release page.^7'):format(data.resource, data.version))
-    end, function(err)
-      print('^3[duff]^7 - '..(err or '^1Unable to Check for Updates.^7'))
-    end)
-  end)
+  local version = get_res_meta('duff', 'version', 0)
+  check_resource_version('duff', version, 'donhulieo')
 end
 
 AddEventHandler('onResourceStart', init_duff)
