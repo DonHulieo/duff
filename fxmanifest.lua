71aa35dabe9093085e8ea361bf49ba3845e2159b
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-12-11 14:27:50 +1100

    chore(fxmanifest): bump version to `1.2.2`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'DonHulieo'
 description 'Don\'s Utility Functions for FiveM'
-version '1.2.1'
+version '1.2.2'
 url 'https://github.com/DonHulieo/duff'
 
 server_script 'server/init.lua'
